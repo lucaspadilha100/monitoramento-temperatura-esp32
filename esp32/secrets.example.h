0d02cf033b1d8925cdfc5ab608d7e2d48aae16c1
lucaspadilha100 <74435274+lucaspadilha100@users.noreply.github.com>
2026-02-03 22:19:04 -0300

    Create secrets.example.h

---
+++ esp32/secrets.example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// copiar esse arquivo e renomear para: secrets.h
+// preencher com os dados.
+
+#define WIFI_SSID   "COLOQUE_AQUI_O_NOME_DO_WIFI"
+#define WIFI_PASS   "COLOQUE_AQUI_A_SENHA_DO_WIFI"
+
+// pegar URL do app web do google app script, usar deploy 
+// o exemplo da URL final: https://script.google.com/macros/s/SEU_ID/exec
+#define SHEETS_URL  "COLOQUE_AQUI_A_URL_DO_APPS_SCRIPT"
